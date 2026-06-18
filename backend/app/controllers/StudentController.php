1194880f543ff3ec56e6cf9e17ee17e6ae18b576
Mário Fernando Gomes Neto <mariosoares20012005@gmail.com>
2026-06-18 19:56:40 +0100

    commit

--- backend/app/controllers/StudentController.php
+++ backend/app/controllers/StudentController.php
@@ -2,15 +2,32 @@
 
 namespace App\controllers;
 
+use App\services\StudentService;
+
 
 class StudentController
 {
 	public function add()
 	{
-		$data = json_decode(file_get_contents("php://input"), true);
+		$student = new StudentService();
 
-		echo json_encode([
-			"data" => $data
-		]);
+		if (empty(json_decode(file_get_contents("php://input"), true))) {
+			http_response_code(400); // Bad Request
+			echo json_encode(["message" => "Dados do aluno não foram fornecidos ou são inválidos."]);
+			return;
+		}
+
+		if ($student->create(json_decode(file_get_contents("php://input"), true))) {
+			http_response_code(201);
+			echo json_encode([
+				"message" => "Aluno registrado com sucesso!"
+			]);
+			return;
+		} else {
+			http_response_code(400);
+			echo json_encode([
+				"message" => "Fala ao registrar aluno!"
+			]);
+		}
 	}
 }
